bde0fa8542841e4f66efac7fa5ee3e77bd80d4c5
Jason Wong <Jason@Jasons-Air.att.net>
2016-08-30 17:50:12 -0700

    method change to recursive

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -4,10 +4,18 @@
 //factorial.cpp
 #include "factorial.h"
 
-int factorial(int n) {
-    int result = 1;
+/*int factorial(int n) {
+   /* int result = 1;
     for (int i = n; i > 0; i--) {
         result *= i;
     }
-    return result;
+    //return result;
+}*/
+
+//using recursion
+int factorial(int n){
+    if(n > 1){
+        return n * factorial(n-1);
+    }
+    return 1;
 }
